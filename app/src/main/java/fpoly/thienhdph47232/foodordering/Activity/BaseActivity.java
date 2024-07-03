e7c17b045ecbbc871377f73659bcd94513dc8769
thien19dev <thien19dev@gmail.com>
2024-07-03 23:23:29 +0700

    new

---
+++ app/src/main/java/fpoly/thienhdph47232/foodordering/Activity/BaseActivity.java
@@ -0,0 +1,18 @@
+package fpoly.thienhdph47232.foodordering.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import fpoly.thienhdph47232.foodordering.R;
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/fpoly/thienhdph47232/foodordering/Activity/IntroActivity.java
+++ app/src/main/java/fpoly/thienhdph47232/foodordering/Activity/IntroActivity.java
@@ -1,6 +1,7 @@
 package fpoly.thienhdph47232.foodordering.Activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +10,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import fpoly.thienhdph47232.foodordering.R;
+import fpoly.thienhdph47232.foodordering.databinding.ActivityIntroBinding;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity extends BaseActivity {
+    ActivityIntroBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_intro);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setVariable();
+    }
+    public void setVariable(){
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
+
     }
 }
