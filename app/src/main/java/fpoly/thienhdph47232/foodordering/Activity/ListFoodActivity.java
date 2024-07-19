e87eec14f20be2fdfd641ecb60f0643363e148be
thien19dev <thien19dev@gmail.com>
2024-07-19 13:10:56 +0700

    Main Activity hoan thien

---
+++ app/src/main/java/fpoly/thienhdph47232/foodordering/Activity/ListFoodActivity.java
@@ -0,0 +1,97 @@
+package fpoly.thienhdph47232.foodordering.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import fpoly.thienhdph47232.foodordering.Domain.Foods;
+import fpoly.thienhdph47232.foodordering.R;
+import fpoly.thienhdph47232.foodordering.databinding.ActivityListFoodBinding;
+
+public class ListFoodActivity extends BaseActivity {
+    ActivityListFoodBinding binding;
+    private RecyclerView.Adapter adapterFoodListView;
+    private int categoryId;
+    private String categoryName;
+    private String searchText;
+    private boolean isSearch;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_list_food);
+        binding = ActivityListFoodBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getIntentExtra();
+        initList();
+    }
+
+    private void initList() {
+        DatabaseReference myRef = database.getReference("Foods");
+        binding.progressBarFoods.setVisibility(View.VISIBLE);
+        ArrayList<Foods> list = new ArrayList<>();
+
+        Query query;
+        if (isSearch){
+            query = myRef.orderByChild("Title").startAt(searchText).endAt(searchText+'\uf8ff');
+        } else {
+            query = myRef.orderByChild("CategoryId").equalTo(categoryId);
+        }
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    for (DataSnapshot issue : snapshot.getChildren()){
+                        list.add(issue.getValue(Foods.class));
+                    }
+                    if (list.size()>0){
+                        binding.foodListView.setLayoutManager(new GridLayoutManager(ListFoodActivity.this, 2));
+                        adapterFoodListView = new Food
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    private void getIntentExtra() {
+        categoryId = getIntent().getIntExtra("CategoryId", 0);
+        categoryName = getIntent().getStringExtra("Category");
+        searchText = getIntent().getStringExtra("text");
+        isSearch = getIntent().getBooleanExtra("isSearch", false);
+
+        binding.titleTxt.setText(categoryName);
+        binding.backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/fpoly/thienhdph47232/foodordering/Adapter/FoodListAdapter.java
@@ -0,0 +1,47 @@
+package fpoly.thienhdph47232.foodordering.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import fpoly.thienhdph47232.foodordering.Domain.Foods;
+import fpoly.thienhdph47232.foodordering.R;
+
+public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.viewholder> {
+    ArrayList<Foods> items;
+    Context context;
+
+    public FoodListAdapter(ArrayList<Foods> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public FoodListAdapter.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View inflate = LayoutInflater.from(context).inflate(R.layout.)
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodListAdapter.viewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class viewholder extends RecyclerView.ViewHolder {
+        public viewholder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
